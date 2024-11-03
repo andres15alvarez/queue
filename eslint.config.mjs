5c9a0dd1281201c0cce200c5bd60ac8b85ebe977
Andres Alvarez <andres15alvarez@gmail.com>
2024-11-03 18:28:55 -0400

    Update config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
   {
-    ignores: [
-        "node_modules"
-    ]
+    ignores: ['node_modules']
   },
   eslintConfigPrettier,
   eslint.configs.recommended,
-  ...tseslint.configs.strict,
-);
+  ...tseslint.configs.strict
+)
